b13b4d76d3829bb5fb2cd70320cd626424a0c6b2
Cloud <cloudcr@gmail.com>
2024-04-05 21:23:03 +0800

    Add sigmoid layer

--- src/layers.rs
+++ src/layers.rs
@@ -1,4 +1,7 @@
 use ndarray::{Array, Dimension};
+use crate::functions::*;
+
+type NNMatrix<D> = Array<f32, D>;
 
 #[derive(Debug)]
 pub struct Relu<D: Dimension> {
@@ -42,6 +45,30 @@ impl<D: Dimension> Relu<D> {
 }
 
 
+struct Sigmoid<D: Dimension> {
+    out: Option<Array<f32, D>>
+}
+
+impl<D: Dimension> Sigmoid<D> {
+    pub fn new() -> Self {
+        Self {
+            out: None
+        }
+    }
+
+    pub fn forward(&mut self, x: &NNMatrix<D>) -> NNMatrix<D> {
+        let out = sigmoid(x);
+        self.out = Some(out.clone());
+        out
+    }
+
+    pub fn backward(&self, dout: &NNMatrix<D>) -> NNMatrix<D> {
+        let out = self.out.as_ref().unwrap();
+
+        dout * (1.0 - out) * out
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,5 +94,25 @@ mod tests {
         let dout_vec = array![[0.3, -0.5, -2.8], [0.1, 0.4, -1.2]];
         let result_vec = array![[0.3, 0.0, -2.8], [0.0, 0.4, 0.0]];
         assert_eq!(relu_layer.backward(&dout_vec), result_vec);
+    }
+
+    #[test]
+    fn test_sigmoid_forward() {
+        // a bit of silly case?
+        let mut sigmoid_layer = Sigmoid::<Ix2>::new();
+        let x_vec = array![
+            [18.0, 0.0, -100.0f32],
+            [-100.0, 50.0, 0.0],
+            ];
+        let result_vec = array![
+            [1.0, 0.5f32, 0.0],
+            [0.0, 1.0, 0.5],
+            ];
+        assert_eq!(sigmoid_layer.forward(&x_vec), result_vec);
+    }
+
+    #[test]
+    fn test_sigmoid_backward() {
+        // TODO: use the regular way of calculating differentials to check the result ?
     }
 }
