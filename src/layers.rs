f3bc3a067bdac8f9cfbaacde9fd0d8f007e7b0a2
Cloud <cloudcr@gmail.com>
2024-04-08 20:39:26 +0800

    Add Affine layer

--- src/layers.rs
+++ src/layers.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array, Dimension};
+use ndarray::{Array, array, Axis, Dimension, Ix1, Ix2};
 use crate::functions::*;
 
 type NNMatrix<D> = Array<f32, D>;
@@ -31,21 +31,23 @@ impl<D: Dimension> Relu<D> {
         }
     }
 
-    pub fn backward(&self, dout: &Array<f32, D>) -> Array<f32, D> {
-        let mut clone_dout = dout.clone();
+    pub fn backward(&self, d_out: &Array<f32, D>) -> Array<f32, D> {
+        let mut clone_d_out = d_out.clone();
         if let Some(ref _mask) = self.mask {
-            clone_dout.zip_mut_with(_mask,
+            clone_d_out.zip_mut_with(_mask,
                  |x_value, bool_value| {
                      if *bool_value { *x_value = 0.0 }; 
                 });
         }
-        clone_dout
+        clone_d_out
     }
     
 }
 
 
+#[derive(Debug)]
 struct Sigmoid<D: Dimension> {
+    #[allow(dead_code)]
     out: Option<Array<f32, D>>
 }
 
@@ -55,19 +57,62 @@ impl<D: Dimension> Sigmoid<D> {
             out: None
         }
     }
+}
 
+impl<D: Dimension> Sigmoid<D> {
     pub fn forward(&mut self, x: &NNMatrix<D>) -> NNMatrix<D> {
         let out = sigmoid(x);
         self.out = Some(out.clone());
         out
     }
 
-    pub fn backward(&self, dout: &NNMatrix<D>) -> NNMatrix<D> {
+    pub fn backward(&self, d_out: &NNMatrix<D>) -> NNMatrix<D> {
         let out = self.out.as_ref().unwrap();
+        out.shape();
 
-        dout * (1.0 - out) * out
+        d_out * (1.0 - out) * out
     }
 }
+
+type NN2Matrix = Array<f32, Ix2>;
+
+struct Affine {
+    bias: NN2Matrix,
+    d_bias: Option<Array<f32, Ix1>>,
+    weight: NN2Matrix,
+    d_weight: Option<NN2Matrix>,
+    x: Option<NN2Matrix>,
+}
+
+impl Affine {
+    pub fn new(weight: NN2Matrix, bias: NN2Matrix) -> Self {
+        Self {
+            bias,
+            d_bias: None,
+            weight,
+            d_weight: None,
+            x: None,
+        }
+    }
+}
+
+impl Affine {
+    pub fn forward(&mut self, x: &NN2Matrix) -> NN2Matrix {
+        self.x = Some(x.clone());
+        let out = x.dot(&self.weight) + &self.bias;
+
+        out
+    }
+
+    pub fn backward(&mut self, d_out: &NN2Matrix) -> NN2Matrix {
+        let d_x = d_out.dot(&self.weight.t());
+        self.d_weight = Some(self.x.as_ref().unwrap().t().dot(d_out));
+        self.d_bias = Some(d_out.sum_axis(Axis(0)));
+
+        d_x
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -91,9 +136,9 @@ mod tests {
     #[test]
     fn test_relu_backward() {
         let relu_layer = Relu { mask: Some(array![[false, true, false], [true, false, true]]) };
-        let dout_vec = array![[0.3, -0.5, -2.8], [0.1, 0.4, -1.2]];
+        let d_out_vec = array![[0.3, -0.5, -2.8], [0.1, 0.4, -1.2]];
         let result_vec = array![[0.3, 0.0, -2.8], [0.0, 0.4, 0.0]];
-        assert_eq!(relu_layer.backward(&dout_vec), result_vec);
+        assert_eq!(relu_layer.backward(&d_out_vec), result_vec);
     }
 
     #[test]
